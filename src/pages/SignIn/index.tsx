e735d72c1bbef0b199bbc4e0afd5dfd5f517c578
Danilo Bandeira <danilobandeira29@gmail.com>
2020-06-04 18:40:32 -0300

    isolated input/button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -11,9 +13,16 @@ const SignIn: React.FC = () => (
       <img src={logo} alt="GoBarber" />
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -28,52 +28,20 @@ export const Content = styled.div`
     h1 {
       margin-bottom: 24px;
     }
-    input {
-      background: #232129;
-      border: 2px solid #232129;
-      border-radius: 10px;
+  }
 
-      width: 100%;
-      padding: 16px;
-      color: #f4ede8;
-      & + input {
-        margin-top: 8px;
-      }
+  a {
+    text-decoration: none;
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    transition: color 0.2s;
 
-      &::placeholder {
-        color: #666360;
-      }
-    }
-    button {
-      background: #ff9000;
-      color: #312e38;
-      border: 0;
-      border-radius: 10px;
-
-      width: 100%;
-      height: 56px;
-      padding: 0 16px;
-      font-weight: 500;
-      margin-top: 16px;
-
-      transition: background-color 0.2s;
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
-    a {
-      text-decoration: none;
-      color: #f4ede8;
-      display: block;
-      margin-top: 24px;
-      transition: color 0.2s;
-
-      &:hover {
-        color: ${shade(0.2, '#f4ede8')};
-      }
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
+
   > a {
     text-decoration: none;
     color: #ff9000;
