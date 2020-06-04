fe5bdcee4ced33eb6fcb17c5cbbc165ed2e77a9d
Danilo Bandeira <danilobandeira29@gmail.com>
2020-06-04 18:52:17 -0300

    added page/styles SignUp

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="nome" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="create">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
