fb4e924756c0f5ee7bf7e95734da373b0fb1391b
Danilo Bandeira <danilobandeira29@gmail.com>
2020-06-04 19:50:46 -0300

    added performant form from unform library

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Form } from '@unform/web';
+
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 
@@ -7,32 +9,38 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logo} alt="GoBarber" />
-      <form>
-        <h1>Faça seu cadastro</h1>
+const SignIn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-        <Input name="nome" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-      <a href="create">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
