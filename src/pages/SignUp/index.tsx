26fc9aa01c79724a1fb7186b1c68e4c06b6f2187
Danilo Bandeira <danilobandeira29@gmail.com>
2020-06-05 16:14:59 -0300

    added usability for input component; object schema validation using yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useCallback } from 'react';
+import * as Yup from 'yup';
 import { Form } from '@unform/web';
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
@@ -10,9 +10,21 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email(),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
