f7d37dfb3478299883fe2057a3c674b43a98c078
Danilo Bandeira <danilobandeira29@gmail.com>
2020-06-14 16:06:28 -0300

    isolated toasted component and implemented timeout

--- src/components/ToastsContainer/index.tsx
+++ src/components/ToastsContainer/index.tsx
@@ -1,38 +1,21 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { ToastMessage, useToast } from '../../hooks/toast';
+import { ToastMessage } from '../../hooks/toast';
 
-import { Container, Toast } from './styles';
+import { Container } from './styles';
+
+import Toast from './Toast';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastsContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
   return (
     <Container>
-      {messages.map(message => {
-        return (
-          <Toast
-            hasDescription={!!message.description}
-            key={message.id}
-            type={message.type}
-          >
-            <FiAlertCircle size={20} />
-
-            <div>
-              <strong>{message.title}</strong>
-              {message.description && <p>{message.description}</p>}
-            </div>
-
-            <button type="button" onClick={() => removeToast(message.id)}>
-              <FiXCircle size={18} />
-            </button>
-          </Toast>
-        );
-      })}
+      {messages.map(message => (
+        <Toast key={message.id} message={message} />
+      ))}
     </Container>
   );
 };
